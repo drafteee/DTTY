edff69cd282b213ca69014beeb30b2043a84acc4
Kozlovski Sergei <kozlovski.sergei.bsuir@gmail.com>
2021-04-13 15:49:02 +0300

    add UWP

--- TimeTrackingYoutrack/TimeTrackingYoutrack/Form1.cs
+++ TimeTrackingYoutrack/TimeTrackingYoutrack/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,15 +8,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TimeTrackingYoutrack.Services;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation.Collections;
 
 namespace TimeTrackingYoutrack
 {
     public partial class Form1 : Form
     {
+        private NotificationService _notificationService;
         public Form1()
         {
             InitializeComponent();
+            _notificationService = new NotificationService();
         }
 
+        
+        private void btnShowNotify_Click(object sender, EventArgs e)
+        {
+            _notificationService.Show();
+        }
     }
 }
---
+++ TimeTrackingYoutrack/TimeTrackingYoutrack/Services/Activator.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation.Collections;
+
+namespace TimeTrackingYoutrack.Services
+{
+    [ClassInterface(ClassInterfaceType.None)]
+    [ComSourceInterfaces(typeof(INotificationActivationCallback))]
+    [Guid("A3E120B9-44C3-447C-831D-81A68078B010"),ComVisible(true)]
+    class Activator : NotificationActivator
+    {
+        public override void OnActivated(string arguments, NotificationUserInput userInput, string appUserModelId)
+        {
+            var a = 5;
+            //if (arguments is ToastNotificationActivatedEventArgs toastActivationArgs)
+            //{
+            //    // Obtain the arguments from the notification
+            //    ToastArguments args = ToastArguments.Parse(toastActivationArgs.Argument);
+
+            //    // Obtain any user input (text boxes, menu selections) from the notification
+            //    ValueSet userInput = toastActivationArgs.UserInput;
+
+            //    // TODO: Show the corresponding content
+            //}
+        }
+    }
+}
