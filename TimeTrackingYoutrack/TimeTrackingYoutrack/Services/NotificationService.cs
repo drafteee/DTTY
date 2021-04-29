3f8c00b6872681788d9cc43af8ecfb800ed63ad5
Сергей Козловский <sergej_-96@mail.ru>
2021-04-29 09:21:16 +0300

    #DTTY-11

--- TimeTrackingYoutrack/TimeTrackingYoutrack/Services/NotificationService.cs
+++ TimeTrackingYoutrack/TimeTrackingYoutrack/Services/NotificationService.cs
@@ -28,6 +28,8 @@ namespace TimeTrackingYoutrack.Services
         public void Show()
         {
             int taskId = 9813;
+
+
             new ToastContentBuilder()
                 .AddArgument("taskId", taskId)
                 .AddText("Запущен таймер задачи _имя_")
